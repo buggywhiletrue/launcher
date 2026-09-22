7477bff9b7bb896f74f12f349483906a1c310e90
Buggy <87579563+buggywhiletrue@users.noreply.github.com>
2026-09-22 12:18:14 +0900

    Update launcher header download links

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -92,16 +92,16 @@ function App() {
                     <h1 className="z-10 flex w-full items-center justify-between bg-black/50 p-4 text-[1rem] font-bold">
                         <div className="flex gap-1">Mushroom Launcher 2</div>
                         <div className="flex h-6 items-center gap-2">
+                            <DownloadSheet />
                             <a
                                 href="https://github.com/buggywhiletrue/launcher/releases/latest"
                                 target="_blank"
                                 rel="noreferrer"
                                 className="rounded-md p-2 hover:bg-gray-200/15"
-                                title="Latest launcher release"
+                                title="런처 업데이트"
                             >
                                 <GithubIcon />
                             </a>
-                            <DownloadSheet />
                             <ModsSheet />
                             <SettingsSheet />
                             <a
